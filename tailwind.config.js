91391eb179731f932591cdb2708191b3b62ce1bd
daft2 <daftdevs@gmail.com>
2022-06-11 23:44:39 +0700

    style: add cusotm colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         dmSans: ["'DM Sans'", "sans-serif"],
-        exo: ["'Exo'", "sans-serif"]
-      }
+        exo: ["'Exo'", "sans-serif"],
+      },
+      colors: {
+        theme: "#170C17",
+      },
     },
   },
   plugins: [],
-}
+};
