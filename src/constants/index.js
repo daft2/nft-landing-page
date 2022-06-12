9f9ba755589c201f0b5eb8d6ed2bdc6732ca6110
daft2 <daftdevs@gmail.com>
2022-06-13 02:44:42 +0700

    chore: create constant placeholder

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const PLACEHOLDER_LIST = [
+  {
+    index: 1,
+    text: 'Create your own customizable NFT in a minutes'
+  },
+  {
+    index: 2,
+    text: 'Play, Interact, and Earn through various activity with different NFT Collection'
+  },
+  {
+    index: 3,
+    text: 'Own your very first tradables NFT to show off to your friends'
+  },
+]
