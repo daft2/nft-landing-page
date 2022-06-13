98c79d16fa9c6a6c2b05d5d941fbd0f8257a3f9a
daft2 <daftdevs@gmail.com>
2022-06-13 15:41:29 +0700

    style: change size

--- src/components/custom-button/CustomButton.tsx
+++ src/components/custom-button/CustomButton.tsx
@@ -9,7 +9,7 @@ const CustomButton = ({
 }) => {
   return (
     <button
-      className={`font-exo font-bold rounded-lg text-sm block w-full lg:text-lg py-3 lg:w-52 lg:py-4 ease-in-hover ${className}`}
+      className={`font-exo font-bold rounded-lg text-sm block w-32 lg:text-lg py-3 lg:w-52 lg:py-4 ease-in-hover ${className}`}
     >
       {children}
     </button>
