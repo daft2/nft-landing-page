0e22154aa7ef15ee8051f874d464c3da61243de9
daft2 <daftdevs@gmail.com>
2022-06-13 16:15:49 +0700

    chore: refactor constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,3 +12,44 @@ export const PLACEHOLDER_LIST = [
     text: 'Own your very first tradables NFT to show off to your friends'
   },
 ]
+
+export const PLACEHOLDER_ROADMAP_1 = [
+  {
+    index: 1,
+    text: 'Launch our own crypto token for staking'
+  },
+  {
+    index: 2,
+    text: 'Participate in the DAO Governing'
+  },
+]
+export const PLACEHOLDER_ROADMAP_2 = [
+  {
+    index: 3,
+    text: 'Launch our own crypto token for staking'
+  },
+  {
+    index: 4,
+    text: 'Participate in the DAO Governing'
+  },
+]
+export const PLACEHOLDER_ROADMAP_3 = [
+  {
+    index: 5,
+    text: 'Launch our own crypto token for staking'
+  },
+  {
+    index: 6,
+    text: 'Participate in the DAO Governing'
+  },
+]
+export const PLACEHOLDER_ROADMAP_4 = [
+  {
+    index: 7,
+    text: 'Launch our own crypto token for staking'
+  },
+  {
+    index: 8,
+    text: 'Participate in the DAO Governing'
+  },
+]
